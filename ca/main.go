5f0237adb63bd1be748d10037bd6aa879ecdeac0
Ciaran Downey <me@ciarand.me>
2015-05-17 23:21:07 -0700

    Reformat document to match final structure

--- ca/main.go
+++ ca/main.go
@@ -8,29 +8,39 @@ import (
 	"os"
 )
 
+// Fire takes a set of CLI arguments and returns an exit code (and maybe an
+// error).
 func fire(args []string) (int, error) {
 	fset := flag.NewFlagSet("", flag.ExitOnError)
 
+	// setup some command line flags
 	var address, certfile, keyfile string
 	fset.StringVar(&address, "address", ":3003", "the address to listen on")
 	fset.StringVar(&keyfile, "keyfile", "rootCA.key", "the keyfile to use")
 	fset.StringVar(&certfile, "certfile", "rootCA.crt", "the certfile to use")
 
+	// attempt to parse the flags
 	if err := fset.Parse(args); err != nil {
 		return 1, err
 	}
 
+	// begin listening
 	fmt.Println("Listening to HTTPS on address: " + address)
 
-	return 1, http.ListenAndServeTLS(address, certfile, keyfile, http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		rw.Header().Set("Content-Type", "text/plain")
+	// this blocks unless the listen fails (in which case we return a failing
+	// exit code and the resulting error).
+	return 1, http.ListenAndServeTLS(address, certfile, keyfile,
+		// create an anonymous http handler
+		http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+			rw.Header().Set("Content-Type", "text/plain")
 
-		if _, err := rw.Write([]byte("Hello world!")); err != nil {
-			log.Println("couldn't write to response: %v", err)
-		}
-	}))
+			if _, err := rw.Write([]byte("Hello from TLS!")); err != nil {
+				log.Printf("couldn't write to response: %v", err)
+			}
+		}))
 }
 
+// run the actual program, handling errors as appropriate.
 func main() {
 	code, err := fire(os.Args)
 	if err != nil {
